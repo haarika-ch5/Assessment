5c1ca243b00879cadae1e8c99e1e9b8f17d211fe
haarika-ch5 <85361778+haarika-ch5@users.noreply.github.com>
2021-06-05 16:00:16 -0500

    Delete Order.cs

--- Models/Order.cs
+++
@@ -1,9 +0,0 @@
-namespace SampleOrders.Models
-{
-    public class Order
-    {
-        public int OrderId { get; set; }
-        public string OrderName { get; set; }
-        public int OrderPrice { get; set; }
-    }
-}
